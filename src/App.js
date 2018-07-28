e95bbb5edfa1c95f4f7bb8cb8e796615fd48596d
Cameron Pavey <cpave3@gmail.com>
2018-07-28 14:36:25 +1000

    :construction: Work on setting up peer connection

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-import { BrowserView, MobileView } from 'react-device-detect';
+import { isBrowser, BrowserView, MobileView } from 'react-device-detect';
+import logo from './logo.svg';
 import './App.css';
+import Peer from 'peerjs';
 import MobileApp from './MobileApp';
 
+/**
+ * Genrate the 'unique' pin for connecting between peers
+ */
+function generatePin () {
+    const min = 0;
+    const max = 9999;
+    return ("0" + (Math.floor(Math.random() * (max - min + 1)) + min)).substr(-4);
+}
+
 class App extends Component {
+
+    constructor(props) {
+      super(props);
+      const id = `${generatePin()}`
+      this.state = {
+        // Store the pin/id for display and reference
+        id: id,
+        
+        // Register the client with the peerjs-server
+        peer: new Peer(id, { host: 'localhost', port: 9000, path: '/', key: 'peerjs' }),
+
+        // Record the client type for easy reference
+        type: isBrowser ? 'browser' : 'mobile',
+
+        messages: [
+          'test',
+          'test2'
+        ],
+      };
+    }
+
     render() {
         return (
             <div className="App">
                 <BrowserView>
-                    <h1> Placeholder for web </h1>
+                  <h1> {this.state.id} </h1>
+                  <div>
+                    {this.state.messages.join(', ')}
+                  </div>
                 </BrowserView>
 
                 <MobileView>
-                    <MobileApp />
+                  <h1> {this.state.id} </h1>
+                  <MobileApp />
                 </MobileView>
             </div>
         );
