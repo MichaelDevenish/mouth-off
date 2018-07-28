73124b96498943e574536b0411ae66253a033cce
LEECH-SURFACE\liamd <liamdidato@gmail.com>
2018-07-28 15:01:14 +1000

    connectinput sends values

--- src/ConnectInput.js
+++ src/ConnectInput.js
@@ -1,8 +1,27 @@
 import React from "react";
 
-export default ({connectHandler}) => (
-    <div>
-        <input />
-        <button onClick={connectHandler} type="button">Connect</button> 
-    </div>
-);
+export default class {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(input) {
+        this.setState(input);
+    }
+
+    render() {
+        const { connectHandler } = this.props; 
+        const { input } = this.state;
+        return (
+            <div>
+                <input/>
+                <button onClick={() => connectHandler(input)} type="button">Connect</button> 
+            </div>
+        )
+        
+    }
+}
