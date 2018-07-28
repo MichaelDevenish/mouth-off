1f9e4e0029ecda05cbd6d664a1f1725bcaa0ca97
LEECH-SURFACE\liamd <liamdidato@gmail.com>
2018-07-28 17:55:30 +1000

    idk

--- src/MobileApp.js
+++ src/MobileApp.js
@@ -25,8 +25,8 @@ class MobileApp extends Component {
             y
         } = this.state
 
-        let top = (y + (2 * parseFloat(event.accelerationIncludingGravity.y).toFixed(1)));
-        let left = (x + (2 * parseFloat(event.accelerationIncludingGravity.x).toFixed(1)) * -1);
+        let top = (y + (2 * parseFloat(event.accelerationIncludingGravity.y).toFixed(1)) * -1);
+        let left = (x + (2 * parseFloat(event.accelerationIncludingGravity.x).toFixed(1)));
 
         const maxWidth = window.innerWidth - 4;
         const maxHeight = window.innerHeight - 4;
