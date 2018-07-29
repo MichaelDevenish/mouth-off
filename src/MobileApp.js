f5603ffef7482cf38f5c429f810b3b30b2903bf8
MichaelDevenish <iammichaeldeve@gmail.com>
2018-07-29 12:26:49 +1000

    Initial mobile refactor

--- src/MobileApp.js
+++ src/MobileApp.js
@@ -1,12 +1,9 @@
 import React, { Component, Fragment } from 'react';
-import DrawingCanvas from './DrawingCanvas';
-import ConnectInput from "./ConnectInput.js";
 
 class MobileApp extends Component {
 
     constructor(props) {
         super(props);
-        this.connectHandler = this.connectHandler.bind(this);
         this.handlePenChange = this.handlePenChange.bind(this);
     }
 
@@ -34,8 +31,8 @@ class MobileApp extends Component {
             event
         });
 
-        let top = (y + (0.2 * parseFloat(event.rotationRate.alpha).toFixed(1)) * -1);
-        let left = (x + (0.2 * parseFloat(event.rotationRate.gamma).toFixed(1)) * -1);
+        let top = (y + (2 * parseFloat(event.accelerationIncludingGravity.y).toFixed(1)) * -1);
+        let left = (x + (2 * parseFloat(event.accelerationIncludingGravity.x).toFixed(1)));
 
         const maxWidth = window.innerWidth - 4;
         const maxHeight = window.innerHeight - 4;
@@ -60,14 +57,6 @@ class MobileApp extends Component {
         }
     }
 
-    connectHandler(stringId) {
-        var conn = this.state.peer.connect(stringId);
-        this.props.handleSetConn(conn);
-        conn.on('open', function(){
-            console.log('connection open', conn);
-            conn.send('Connection Established');
-        });
-    }
 
     handlePenChange(penDown) {
         this.setState({penDown});
@@ -86,25 +75,7 @@ class MobileApp extends Component {
         } = this.state
 
         return (
-            <Fragment>
-                <ConnectInput connectHandler={this.connectHandler} />
                 <button onMouseDown={() => this.handlePenChange(true)} onMouseUp={() => this.handlePenChange(false)} type="button">Draw.</button> 
-                <div>
-                    <h2>Acceleration</h2>
-                    <p>X: {parseFloat(this.state.accel.x).toFixed(2)}</p>
-                    <p>Y: {parseFloat(this.state.accel.y).toFixed(2)}</p>
-                    <p>Z: {parseFloat(this.state.accel.z).toFixed(2)}</p>
-                    <h2>Rotation</h2>
-                    <p>Alpha: {parseFloat(this.state.rotate.alpha).toFixed(2)}</p>
-                    <p>Beta: {parseFloat(this.state.rotate.beta).toFixed(2)}</p>
-                    <p>Gamma: {parseFloat(this.state.rotate.gamma).toFixed(2)}</p>
-                </div>
-                <DrawingCanvas
-                    left={left}
-                    top={top}
-                    penDown
-                />
-            </Fragment>
         );
     }
 }
